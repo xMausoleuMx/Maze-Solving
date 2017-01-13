03d68ca38472df9b34cd9413b47efcc260dae4ca
xMausoleuMx <j0hntw1ll1ams95@gmail.com>
2017-01-12 21:28:30 -0600

    Added the pathto Function, does not work yet but on its way there.

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,7 @@ void readMap(string filename);
 void printMap();
 vector<vector<int>> getCoords();
 void initialSolve(vector<vector<int>> coords);
+int pathTo(vector<int> start, vector<int> end, int type);
 
 vector<vector<char>> maze;
 
@@ -46,10 +47,23 @@ vector<vector<int>> getCoords()
 void initialSolve(vector<vector<int>> coords)
 {
 	vector<int> distances;
+	vector<vector<int>> bestOrder;
+	bestOrder.push_back(coords[0]);
+	int minDistance = INT_MAX, holder = 0, smallest;
 	for (int i = 0; i < coords.size(); i++)
 	{
-
-
+		for (int z = i+1; z < coords.size(); z++)
+		{
+			holder = pathTo(coords[i], coords[z], 0);
+			if (holder < minDistance)
+			{
+				minDistance = holder;
+				smallest = z;
+			}
+		}
+		bestOrder.push_back(coords[smallest]);
+		coords.insert(coords.begin()+i + 1, coords[smallest]);
+		coords.erase(coords.begin()+smallest+1);
 	}
 }
 
@@ -77,4 +91,23 @@ void printMap()
 			std::cout << maze[i][z];
 		std::cout << endl;
 	}
+}
+
+int pathTo(vector<int> start, vector<int> end, int type)
+{
+	int distances [] = {0,0,0,0};
+	if (start[1] == end[1] && start[2] == end[2])
+		return 0;
+	if ((start[1] + 1 < maze.size()) && (maze[start[1] + 1][start[2]] != '#'))
+		distances[0] = pathTo({ start[0], start[1] + 1, start[2] }, end, type);
+	else if ((start[2] + 1 <maze[0].size()) && (maze[start[1]][start[2]+1] != '#'))
+		distances[1] = pathTo({ start[0], start[1], start[2] + 1 }, end, type);
+	else if ((start[1] - 1 >= 0 ) && (maze[start[1] - 1][start[2]] != '#'))
+		distances[2] = pathTo({ start[0], start[1] - 1, start[2] }, end, type);
+	else if ((start[2] - 1 >= 0 ) && (maze[start[1]][start[2]-1] != '#'))
+		distances[3] = pathTo({ start[0], start[1], start[2] - 1 }, end, type);
+	else
+		return -1;
+
+	if()
 }
